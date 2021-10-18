6543bee612b2c9de226e29aaeda76506b957eab4
agudelodiego <43055497+agudelodiego@users.noreply.github.com>
2021-10-17 22:47:19 -0500

    Arreglo de algunos errores

--- codigoFuente/codigoFuente.ino
+++ codigoFuente/codigoFuente.ino
@@ -53,8 +53,8 @@ void setup() {
   Wire.begin();
 
   /*INICIALIZACION DE SERVOMOTORES*/
-  hombro.attach(6, 1000, 2000);
-  base.attach(7, 1000, 2000);
+  base.attach(6, 1000, 2000);
+  hombro.attach(7, 1000, 2000);
   codo.attach(8, 1000, 2000);
   manoX.attach(9, 1000, 2000);
   manoY.attach(10, 1000, 2000);
@@ -73,7 +73,7 @@ void setup() {
 void loop() {
 
   /*-----ENTRAR CONTROLAR EL BRAZO DE MANERA MANUAL------*/
-  if (digitalRead(operacion) == 1){
+  if (digitalRead(operacion) == HIGH){
     
     /*ENVIAR MENSAJE AL ESCLAVO 1*/
     Wire.beginTransmission(1);
@@ -87,7 +87,7 @@ void loop() {
 
 
   /*-------ENTRAR A RUTINA PREPROGRAMA DEL BRAZO--------*/
-  if(digitalRead(operacion) == 0){
+  if(digitalRead(operacion) == LOW){
 
     /*ENVIAR MENSAJE AL ESCLAVO 1*/
     Wire.beginTransmission(1);
@@ -112,12 +112,12 @@ void manual(){
     if (analogRead(0) < 200 && p_base < 180) {
       p_base++;
       base.write(p_base);
-      delay(2);
+      delay(7);
     }
     if (analogRead(0) > 700 && p_base > 0) {
       p_base--;
       base.write(p_base);
-      delay(2);
+      delay(7);
     }
     /*----------------------------------------------------*/
 
@@ -126,12 +126,12 @@ void manual(){
     if (analogRead(1) < 200 && p_hombro < 180) {
       p_hombro++;
       hombro.write(p_hombro);
-      delay(2);
+      delay(7);
     }
     if (analogRead(1) > 700 && p_hombro > 0) {
       p_hombro--;
       hombro.write(p_hombro);
-      delay(2);
+      delay(7);
     }
     /*----------------------------------------------------*/
 
@@ -140,12 +140,12 @@ void manual(){
     if (analogRead(15) < 200 && p_codo < 180) {
       p_codo++;
       codo.write(p_codo);
-      delay(2);
+      delay(7);
     }
     if (analogRead(15) > 700 && p_codo > 0) {
       p_codo--;
       codo.write(p_codo);
-      delay(2);
+      delay(7);
     }
     /*----------------------------------------------------*/
 
@@ -154,12 +154,12 @@ void manual(){
     if (analogRead(14) < 200 && p_manoX < 180) {
       p_manoX++;
       manoX.write(p_manoX);
-      delay(2);
+      delay(7);
     }
     if (analogRead(14) > 700 && p_manoX > 0) {
       p_manoX--;
       manoX.write(p_manoX);
-      delay(2);
+      delay(7);
     }
     /*----------------------------------------------------*/
 
@@ -168,42 +168,46 @@ void manual(){
     /*---------------------SERVO MANO Y-------------------*/
     if (digitalRead(suma_manoY) == LOW){
       p_manoY++;
-      delay(2);
-      if (p_manoY >= 180){
-        p_manoY = 180;
+      delay(7);
+      if (p_manoY > 170){
+        p_manoY = 170;
       }
+      manoY.write(p_manoY);
     }
     if (digitalRead(resta_manoY) == LOW){
       p_manoY--;
-      delay(2);
-      if (p_manoY <= 0){
-        p_manoY = 0;
+      delay(7);
+      if (p_manoY < 10){
+        p_manoY = 10;
       }
+      manoY.write(p_manoY);
     }
-    manoY.write(p_manoY);
+    //manoY.write(p_manoY);
     /*----------------------------------------------------*/
 
 
     /*------------------SERVO PINZA-----------------------*/
     if (digitalRead(suma_pinza) == LOW){
       p_pinza++;
-      delay(2);
+      delay(7);
       if (p_pinza >= 90){
         p_pinza = 90;
       }
+      pinza.write(p_pinza);
     }
     if (digitalRead(resta_pinza) == LOW){
       p_pinza--;
-      delay(2);
-      if (p_pinza <= 0){
-        p_pinza = 0;
+      delay(7);
+      if (p_pinza < 10){
+        p_pinza = 10;
       }
+      pinza.write(p_pinza);
     }
-    pinza.write(p_pinza);
+    //pinza.write(p_pinza);
     /*----------------------------------------------------*/
 
     /*----------------IR AL MODO AUTO---------------------*/
-    if(digitalRead(operacion) == 0){
+    if(digitalRead(operacion) == LOW){
        break;  
     }
     /*----------------------------------------------------*/
@@ -225,13 +229,13 @@ void inicial(){
   if(p_base < 90){
     for(p_base; p_base <= 90; p_base++){
       base.write(p_base);
-      delay(2);
+      delay(7);
     }
   }
   if(p_base > 90){
     for(p_base; p_base >= 90; p_base--){
       base.write(p_base);
-      delay(2);
+      delay(7);
     }
   }
   /*------------------------------------------------*/
@@ -241,13 +245,13 @@ void inicial(){
   if(p_hombro < 90){
     for(p_hombro; p_hombro <= 90; p_hombro++){
       hombro.write(p_hombro);
-      delay(2);
+      delay(7);
     }
   }
   if(p_hombro > 90){
     for(p_hombro; p_hombro >= 90; p_hombro--){
       hombro.write(p_hombro);
-      delay(2);
+      delay(7);
     }
   }
   /*--------------------------------------------------*/
@@ -257,13 +261,13 @@ void inicial(){
   if(p_codo < 90){
     for(p_codo; p_codo <= 90; p_codo++){
       codo.write(p_codo);
-      delay(2);
+      delay(7);
     }
   }
   if(p_codo > 90){
     for(p_codo; p_codo >= 90; p_codo--){
       codo.write(p_codo);
-      delay(2);
+      delay(7);
     }
   }
   /*---------------------------------------------------*/
@@ -273,13 +277,13 @@ void inicial(){
   if(p_manoX < 90){
     for(p_manoX; p_manoX <= 90; p_manoX++){
       manoX.write(p_manoX);
-      delay(2);
+      delay(7);
     }
   }
   if(p_manoX > 90){
     for(p_manoX; p_manoX >= 90; p_manoX--){
       manoX.write(p_manoX);
-      delay(2);
+      delay(7);
     }
   }
   /*--------------------------------------------------*/
@@ -289,13 +293,13 @@ void inicial(){
   if(p_manoY < 90){
     for(p_manoY; p_manoY <= 90; p_manoY++){
       manoY.write(p_manoY);
-      delay(2);
+      delay(7);
     }
   }
   if(p_manoY > 90){
     for(p_manoY; p_manoY >= 90; p_manoY--){
       manoY.write(p_manoY);
-      delay(2);
+      delay(7);
     }
   }
   /*--------------------------------------------------*/
@@ -305,13 +309,13 @@ void inicial(){
   if(p_pinza < 90){
     for(p_pinza; p_pinza <= 90; p_pinza++){
       pinza.write(p_pinza);
-      delay(2);
+      delay(7);
     }
   }
   if(p_pinza > 90){
     for(p_pinza; p_pinza >= 90; p_pinza--){
       pinza.write(p_pinza);
-      delay(2);
+      delay(7);
     }
   }
   /*---------------------------------------------------*/
